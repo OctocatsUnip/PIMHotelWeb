8d47fdbf73aa3607448dbea3dfd78361ddfa3413
Thayane Bomfim <thayane_bomfims@hotmail.com>
2021-09-21 22:58:51 -0300

    Componentização check e ajuste home

---
+++ PIM.Site/Views/Shared/Components/Check/CheckViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PIM.Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIM.Site.Views.Shared.Components.Check
+{
+    public class CheckViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var check = new List<CheckViewModel>();
+
+            check.Add(new CheckViewModel { Titulo = "Check-in", Icone = "fa-calendar-alt" });
+            check.Add(new CheckViewModel { Titulo = "Check-out", Icone = "fa-calendar-alt" });
+
+            return View(check);
+        }
+    }
+}
