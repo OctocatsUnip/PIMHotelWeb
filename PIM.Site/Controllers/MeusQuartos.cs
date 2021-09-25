20caa6974581af4bc9d220e1fae0c30d9fd921da
nicozzi <nickpizzoni@outlook.com>
2021-09-24 22:16:37 -0300

    Finalizada pagina meus quartos

---
+++ PIM.Site/Controllers/MeusQuartos.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIM.Site.Controllers
+{
+    public class MeusQuartos : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
