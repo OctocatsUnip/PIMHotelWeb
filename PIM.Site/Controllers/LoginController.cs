52c06eebebaf92eed09af84c4938b3e2e184270d
Thayane Bomfim <thayane_bomfims@hotmail.com>
2021-09-29 22:59:31 -0300

    Finalizacao contato e pag login

---
+++ PIM.Site/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIM.Site.Controllers
+{
+    public class LoginController : Controller
+    {
+        [Route("logar")]
+        public IActionResult SingIn()
+        {
+            return View();
+        }
+    }
+}
