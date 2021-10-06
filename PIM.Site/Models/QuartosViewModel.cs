1158b8ea0eafde29931dd809b6184a7e7d693fb3
Thayane Bomfim <thayane_bomfims@hotmail.com>
2021-10-06 00:54:51 -0300

    componentizacao carousel home

---
+++ PIM.Site/Models/QuartosViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PIM.Site.Models
+{
+    public class QuartosViewModel
+    {
+    }
+}
